0afb2f39425c321de67b8da2b7879073ebf2480c
K. Scott Tripp <ktripp@umich.edu>
2012-10-19 00:40:59 -0400

    Updated TLE and transponder data storage
    TLEs are now split up into single tle files and stored in
    configDir/tle.  Transponder data is in configDir/sats.

--- src/libTLE.py
+++ src/libTLE.py
@@ -10,8 +10,11 @@ import sys
 
 # SplitFile(filename) takes in a TLE file and splits it into
 # files each containing a single TLE.
-def SplitFile(filename):
+def SplitFile(filename, name_flag=1, outputDir=''):
 	tle_master = open(filename, 'r')
+
+	if outputDir == '':
+		outputDir = '.'
 
 	while True:
 		line = tle_master.readline().rstrip()
@@ -19,10 +22,20 @@ def SplitFile(filename):
 		if line == '':
 			break
 
-		tle = open(line + '.txt', 'w')
-		tle.write(line + '\n')
-		tle.write(tle_master.readline().rstrip() + '\n')
-		tle.write(tle_master.readline().rstrip() + '\n')
+		sat_name = (line + '\n')
+		line1 = (tle_master.readline().rstrip() + '\n')
+		line2 = (tle_master.readline().rstrip() + '\n')
+
+		sat_num = line1[2:7] #From TLE specification
+		
+		if name_flag:
+			tle = open(outputDir + "/" + line + '.txt', 'w')
+		else:
+			tle = open(outputDir + "/" + str(sat_num) + '.txt', 'w')
+
+		tle.write(sat_name)
+		tle.write(line1)
+		tle.write(line2)
 		tle.close()
 
 	tle_master.close()
