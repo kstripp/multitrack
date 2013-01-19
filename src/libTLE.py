a6d9242b598d73d16904d8de81f729b143c1ebe0
K. Scott Tripp <ktripp@umich.edu>
2013-01-19 10:52:22 -0500

    Changed tleupdate call to be automatic
    Moved in to a function in libTLE which uses a static
    variable to maintain knowledge of update interval.
    
    Closes #3.

--- src/libTLE.py
+++ src/libTLE.py
@@ -5,8 +5,13 @@
 # K. Scott Tripp <ktripp@umich.edu>
 # September 2012
 
-import sys
+# multitrack configuration variables
+import config
 
+# python standard libraries
+import sys
+import os
+import time
 
 # SplitFile(filename) takes in a TLE file and splits it into
 # files each containing a single TLE.
@@ -39,6 +44,30 @@ def SplitFile(filename, name_flag=1, outputDir=''):
 		tle.close()
 
 	tle_master.close()
+
+#####################################################################
+# update()
+#####################################################################
+def update(tleDir, conf):
+	"""
+	update(tleDir, conf) --> none
+	checkes the elapsed time since the last TLE update,
+	and runs an update if it has been longer than 24 hours
+	"""
+
+	interval = 86400 # seconds per day
+
+	if time.time() - update.last_update > interval:
+
+		# TODO: if tleupdate did *not* succeed, last_update should
+		# not be updated.
+		os.system("tleupdate -d " + tleDir)
+		SplitFile(tleDir + "/" + conf.get("TLE", "tle"), 0, tleDir)
+		update.last_update = time.time()
+
+# initialize last_update timestamp.
+# this forces it to run on first call.
+update.last_update = 0
 
 if __name__ == "__main__":
 
